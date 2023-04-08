93394ab9c038b5c634a50d75834329be4a11b604
ferbonelli <bonelli.fernando@gmail.com>
2023-04-08 12:20:41 -0300

    Login  mal

--- MiPorfolio/src/app/componentes/login/login.component.ts
+++ MiPorfolio/src/app/componentes/login/login.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/servicios/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   usuario: string = '';
   clave: string = '';
