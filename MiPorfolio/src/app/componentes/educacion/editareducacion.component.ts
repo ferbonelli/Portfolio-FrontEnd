3419b2e68c4102cf2ebfce85508509e3ec509ee8
ferbonelli <bonelli.fernando@gmail.com>
2023-05-08 00:22:23 -0300

    Editar educacion ok

--- MiPorfolio/src/app/componentes/educacion/editareducacion.component.ts
+++ MiPorfolio/src/app/componentes/educacion/editareducacion.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Educacion } from 'src/app/model/educacion.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-// Importo servicios
+
+// Lo necesito para Educacion
+import { Educacion } from 'src/app/model/educacion.model';
 import { EducacionService } from 'src/app/servicios/educacion.service';
 
 @Component({
@@ -13,17 +15,53 @@ import { EducacionService } from 'src/app/servicios/educacion.service';
 
 export class EditareducacionComponent implements OnInit {
 
-  educacionEditar: Educacion = new Educacion(0,'','','','','',1);
+  // Declaraciones
+  formularioEducacion: FormGroup;
+  educacionEditar: Educacion = new Educacion(0,'','','','','',0);
 
-constructor (
+constructor ( private formBuilder:FormBuilder,
               private datosEducacion:EducacionService,
               private rutaActiva:ActivatedRoute,
               private ruta:Router
-){}
+            ){
+              this.formularioEducacion=this.formBuilder.group(
+                {
+                  institucion: ['',[Validators.required]],
+                  titulo: ['',[Validators.required]],
+                  url_logoinst: ['',[Validators.required]],
+                  fecha_inicio: ['',[Validators.required,
+                    Validators.pattern (/^(0?[1-9]|[12][0-9]|3[01])[\/-](0?[1-9]|1[012])[\/-]\d{4}$/)
+                    ]],
+                  fecha_final: ['',[Validators.required,
+                    Validators.pattern (/^(0?[1-9]|[12][0-9]|3[01])[\/-](0?[1-9]|1[012])[\/-]\d{4}$/)
+                    ]],
+                }
+              )
+            }
 
 ngOnInit(){
   this.traerEducacion();
   
+}
+
+get Institucion(){
+  return this.formularioEducacion.get('institucion');
+}
+
+get Titulo() {
+  return this.formularioEducacion.get('titulo');
+}
+
+get Url_logoinst() {
+  return this.formularioEducacion.get('url_logoinst');
+}
+
+get Fecha_inicio(){
+  return this.formularioEducacion.get('fecha_inicio');
+}
+
+get Fecha_final() {
+  return this.formularioEducacion.get('fecha_final');
 }
 
 traerEducacion() {
@@ -32,26 +70,24 @@ traerEducacion() {
     this.datosEducacion.obtenerEducacion(id).subscribe(
       data =>{
         this.educacionEditar = data;
-        
-        
-      }, err =>{
-        alert("Error al modificar la educacion");
-        this.ruta.navigate(['/portfolio']);
+               
       }
     )
 }
 
-onUpdate() {
+onUpdate(event: Event) {
   
-  this.datosEducacion.actualizarEducacion(this.educacionEditar).subscribe(
-    data => {
-      alert("Se actualizó la educacion");
+  this.datosEducacion.actualizarEducacion(this.educacionEditar).subscribe({
+    next: data => {
+      alert("Se actualizó Educación");
       this.ruta.navigate(['/portfolio']);
-    }, err =>{
-       alert("Error al modificar la educacion");
+    }, 
+    
+    error: error =>{
+       alert("Error al modificar Educación");
        this.ruta.navigate(['/portfolio']);
     }
-  )
+  })
 
 }
 
