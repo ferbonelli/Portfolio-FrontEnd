3963b6cacaa6dcdfdfe65dc9ca938dc2f70bec65
ferbonelli <bonelli.fernando@gmail.com>
2023-05-09 23:06:00 -0300

    todo ok

--- MiPorfolio/src/app/componentes/banner/editarbanner.component.ts
+++ MiPorfolio/src/app/componentes/banner/editarbanner.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
-import { Persona } from 'src/app/model/persona.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-// Importo servicios
+
+// Lo necesito para los datos de la persona
 import { PersonaService } from 'src/app/servicios/persona.service';
+import { Persona } from 'src/app/model/persona.model';
 
 
 @Component({
@@ -15,16 +17,42 @@ export class EditarbannerComponent implements OnInit {
 
   // Declaraciones
   personaEditar: Persona = new Persona(0,'','','','','','','','','');
+  formularioBanner: FormGroup;
  
 
-  constructor(
-    private datosPersona:PersonaService,
-    private rutaActiva:ActivatedRoute,
-    private ruta:Router
-  ){}
+  constructor(private formBuilder:FormBuilder,
+              private datosPersona:PersonaService,
+              private rutaActiva:ActivatedRoute,
+              private ruta:Router
+            ){
+              this.formularioBanner=this.formBuilder.group(
+                {
+                  nombre: ['',[Validators.required]],
+                  apellido: ['',[Validators.required]],
+                  profesion: ['',[Validators.required]],
+                  url_foto: ['',[Validators.required]],
+                }
+              )
+            }
 
 ngOnInit(): void {
   this.traerPersona();
+}
+
+get Nombre(){
+  return this.formularioBanner.get('nombre');
+}
+
+get Apellido(){
+  return this.formularioBanner.get('apellido');
+}
+
+get Profesion(){
+  return this.formularioBanner.get('profesion');
+}
+
+get Url_foto(){
+  return this.formularioBanner.get('url_foto');
 }
 
 traerPersona() {
@@ -32,26 +60,23 @@ traerPersona() {
 
   this.datosPersona.obtenerPersona(id).subscribe(
     data =>{
-      this.personaEditar = data;
-            
-    }, err =>{
-      alert("Error al modificar el banner");
-      this.ruta.navigate(['/portfolio']);
+      this.personaEditar = data;    
     }
   )
 }
 
-onUpdate() {
+onUpdate(event: Event) {
 
-this.datosPersona.actualizarPersona(this.personaEditar).subscribe(
-  data => {
+this.datosPersona.actualizarPersona(this.personaEditar).subscribe({
+  next: data => {
     alert("Se actualizó el banner");
     this.ruta.navigate(['/portfolio']);
-  }, err =>{
+  },
+  error: error =>{
      alert("Error al modificar el banner");
      this.ruta.navigate(['/portfolio']);
   }
-)
+})
 
 }
 
